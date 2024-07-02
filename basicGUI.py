a0f354403ee2b51da0064f1239da8f2375c1bcef
AnishN <58618493+Cravinghydra@users.noreply.github.com>
2024-07-01 21:24:35 -0400

    Add files via upload

--- basicGUI.py
+++ basicGUI.py
@@ -1,27 +1,41 @@
 from tkinter import *
 import datetime
-import subprocess
 
 root = Tk()
 
 root.geometry('760x760')
+
 root.counter = 0
 
-
 def clicked():
-    root.counter += 50
-    value.config(text= str(root.counter))
-    with open("time.txt", 'a') as file:
-        file.write(str(datetime.datetime.now()))
-        file.write("\n")
+    root.counter += 1
+    if root.counter % 2:
+        state.config(text = "saving")
+    else:
+        state.config(text = "idle")
     
-value = Label(root, height = 2, width = 10, text = "0", fg="white", bg = "black")
-value.pack(side=LEFT, padx=10, pady=10)
+display = Label(root, height = 2, width = 10, text = "0", fg="white", bg = "black")
+display.pack(side=LEFT, padx=10, pady=10)
 
-button = Button(root, text="+50", command = lambda:clicked())
+state = Label(root, height = 2, width = 10, text = "0", fg="white", bg = "black")
+state.pack()
+
+button = Button(root, text="save", command = lambda:clicked())
 button.pack(side=LEFT, padx=10, pady=10)
 
+current_datettime = datetime.datetime.now().strftime("%m-%d %H-%M-%S")
+str_current_datetime = str(current_datettime)
+file_name = str_current_datetime+".txt"
 
-#root.after(1000, open_time)
+with open(file_name, 'w') as file:
+    file.write("")
+
+def time_update():
+    with open(file_name, 'a') as file:
+        file.write(str_current_datetime + "\n")
+    root.after(1000, time_update)
+
+
+root.after(1000, time_update)
 
 root.mainloop()
